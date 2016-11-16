fbfb142adebc1f18c737c7365b5a397afc3ba84f
holgus103 <kubasuchan@hotmail.com>
2016-11-16 01:33:11 +0100

    Inserting unlimited dropdowns for task assignment + Diagrams

--- LogMyWork/Scripts/Tasks.js
+++ LogMyWork/Scripts/Tasks.js
@@ -1,12 +1,18 @@
 ﻿$(document).ready(function () {
-    $("#User_0__Email").change(function () {
-
-    });
+    $("#Users_0__Id").change(handleSelectChange);
 });
+
 function createTask(input) {
     if (input > 0) {
         var taskName = $("#Name").val();
         $(taskNodeTemplate.replace(/TASK_NAME/g, taskName).replace(/TASK_ID/g, input)).insertBefore("#AddTaskPnl");
         }
+}
+
+function handleSelectChange() {
+    if ($(this).val() == "") return;
+    var arr = $("#AddTaskPnl  .form-group")
+    $($("#AddTaskPnl  .form-group")[1]).clone().insertAfter(arr[arr.length - 2])
+    $("select").last().change(handleSelectChange);
 }
 
