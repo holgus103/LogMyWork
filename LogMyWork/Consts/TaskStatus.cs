7587cee5584e38beb2be97663055cd56a27e9a19
holgus103 <kubasuchan@hotmail.com>
2016-11-23 16:59:04 +0100

    Tasks listing
    - Added a partial view for task listing
    - Added sum to tracked entries view
    - Added TaskStatus enum

---
+++ LogMyWork/Consts/TaskStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LogMyWork.Consts
+{
+    public enum TaskStatus
+    {
+        Created,
+        Assigned,
+        InProgress,
+        CurrentlyInProgress,
+        AwaitingFeedback,
+        Finished
+    }
+}
