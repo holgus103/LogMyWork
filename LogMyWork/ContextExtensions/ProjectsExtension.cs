fb40469bf29550580457c8836ed16c1ab62bdcf3
holgus103 <kubasuchan@hotmail.com>
2016-12-02 00:08:00 +0100

    Missing file
    - Missing ProjectsExtension.cs

--- LogMyWork/ContextExtensions/ProjectsExtension.cs
+++ LogMyWork/ContextExtensions/ProjectsExtension.cs
@@ -1,4 +1,5 @@
-﻿using LogMyWork.Models;
+﻿using LogMyWork.Consts;
+using LogMyWork.Models;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
@@ -16,6 +17,21 @@ namespace LogMyWork.ContextExtensions
        .Include(x => x.Roles.Select(r => r.User))
        .ToList()
        .SelectMany(x => x.Roles.Select(r => new KeyValuePair<object, string>(r.User.Id, r.User.Email)));
+        }
+
+        public static bool HasProjectAccess(this LogMyWorkContext context, int projectID, string userID)
+        {
+            return context.ProjectRoles.Where(r => r.ProjectID == projectID && r.UserID == userID).Count() > 0;
+        }
+
+        public static bool HasProjectRole(this LogMyWorkContext context, int projectID, string userID, Role role)
+        {
+            return context.ProjectRoles.Where(r => r.ProjectID == projectID && r.UserID == userID && r.Role == role).Count() > 0;
+        }
+
+        public static ProjectRole GetUserRoleForProject(this LogMyWorkContext context, int projectID, string userID)
+        {
+            return context.ProjectRoles.Where(r => r.ProjectID == projectID && r.UserID == userID).FirstOrDefault();
         }
     }
 }
